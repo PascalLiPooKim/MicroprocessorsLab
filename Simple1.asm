78394e757b33340796d429702f7c59e1c9daa61a
pwl17 <pwl17@skbl-422-009.ph.ic.ac.uk>
2020-01-13 16:56:28 +0000

    Added FSR

--- Simple1.asm
+++ Simple1.asm
@@ -7,6 +7,8 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	lfsr FSR0, 0x50    ; TODO: Create counter for this memory locationload address 0x06 into FSR0
+			; load 0x10 into W
 	movlw 	0x0
 	movwf	TRISB, ACCESS	    ; Port C all outputs
 	bra 	test
@@ -14,6 +16,10 @@ loop	movff 	0x06, PORTB
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	
+	;
+	;movlw	0x00
+	;movwf	0x08, ACCESS
+	call save_fsr
 	
 	; delay speed of counter
 	movlw	0x10
@@ -23,12 +29,10 @@ test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	
 	movlw 	0x63
 	cpfsgt 	0x06, ACCESS
-	bra 	loop
+	bra 	loop	; Not yet finished goto start of loop again
 	
 	
-	
-	
-			    ; Not yet finished goto start of loop again
+	goto 	0x0		    ; Re-run program from start
 	
 	; a delay subroutine
 delay	decfsz	0x20
@@ -43,7 +47,27 @@ delay	decfsz	0x20
 extra_delay decfsz  0x30
 	    bra extra_delay
 	    return
+	    
+save_fsr incf    PREINC0
+			
 	
-	goto 	0x0		    ; Re-run program from start
+	    ;incf INDF0, f  ; adds W to contents of address 0x140	    
+	    incf    PREINC0 
+	    
+	    movlw   0x02
+	    movwf   POSTINC0
+	    
+;	    movlw   0x03
+;	    movwf   POSTINC0
+;	    
+;	    movlw   0x04
+;	    movwf   POSTINC0
+;	    ;addwf 0x60
+	    
+	 lfsr FSR1, 0x60
+	    movff   INDF1,  0x65
+	    ; puts result back into address 0x140
+	    return
+	
 	
 	end
